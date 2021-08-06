d31d4ab98540b49012696b61f7d58850723c1307
Sachin Verma <vermas7008@gmail.com>
2021-08-07 02:02:09 +0530

    yeah this is my first commit

---
+++ lib/screens/results_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
+import 'package:bmi_calculator/components/bottom_button.dart';
+
+class ResultsPage extends StatelessWidget {
+
+   ResultsPage({@required this.bmiResult,@required this.interpretation,@required this.resultText});
+
+   final String bmiResult;
+   final String resultText;
+   final String interpretation;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'Your Result',
+                textAlign: TextAlign.center,
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(colour: kActiveCardColour,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(resultText.toUpperCase(),style: resultTextstyle,),
+                  Text(bmiResult,style: kBMITextStyle,),
+                  Text(interpretation,textAlign: TextAlign.center,style: kBodyTextStyle ,),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+            buttonTitle: 'RE-CALCULATE',
+            onTap: (){
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
